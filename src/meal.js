99d7a9aa421988eb961190a3c52158882424f0f6
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-07-03 15:19:03 -0600

    Complete meal functions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,60 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+  return "Delicious " + food;
+  //var menuItemName = nameMenuItem("Pizza");
+  //var burritoItem = nameMenuItem("Burrito");
+  //var sushiItem = nameMenuItem("Pizza");
+  //var tacoItem = nameMenuItem("Taco");
+  //var frenchToastItem = nameMenuItem("French Toast") //added new code here
+  //these files are instantiated in the test file
+}
+
+function createMenuItem(foodName, dollarPrice, itemType) {
+  var menuItem = {
+    name: foodName,
+    price: dollarPrice,
+    type: itemType
+  };
+  return (menuItem);
+}
+
+function addIngredients(food, ingredients) {
+  if (ingredients.includes(food) == false) {
+    ingredients.push(food);
+  }
+}
+
+function formatPrice(initialPrice) {
+  //return "$" + initialPrice.toString(); //concatenation
+  return `$${initialPrice}`;      //interpolation
+}
+
+function decreasePrice(price) {
+ return price * .9;
+}
+
+function createRecipe(title, ingredients, menuItemtype) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemtype
+  };
+  return recipe;
 
 }
+
+
+
+
+
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
