aa20f504466b9e912681c1df8bd050bbb52fd2a3
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-07-04 11:15:15 -0600

    Complete listItems function

--- src/order.js
+++ src/order.js
@@ -5,13 +5,50 @@ function takeOrder(order, deliveryOrders) {
   }
 }
 
+function refundOrder(orderNumber, deliveryOrdersArray) {
+  var indexToRemove; //declared but undefined
+  for (var i = 0; i < deliveryOrdersArray.length; i++) {
+    var currentOrder = deliveryOrdersArray[i];
+    if (currentOrder.orderNumber === orderNumber) {
+      indexToRemove = i;
+    }
+  }
+  deliveryOrdersArray.splice(indexToRemove, 1);
+}
 
+// var keptOrders = [];
+// for (var i = 0; i < deliveryOrdersArray.length; i++) {
+//   var currentOrder = deliveryOrdersArray[i];
+//   if (currentOrder.orderNumber != orderNumber) {
+//     keptOrders.push(currentOrder) //push current order to kept orders
+//   }
+// }
+// console.log(keptOrders)
+//this for loop is the equivalent of a filter prototype method
 
+//declare variable called indexToRemove & assign to null
+//loop through deliveryOrdersArray
+//if currentOrder.orderNumber === orderNumber then
+//indextoRemove is reassigned to i
+
+function listItems(deliveryOrderItemsArray) {
+  var emptyString = ''
+  //var emptyArray = []
+  for (var i = 0; i < deliveryOrderItemsArray.length; i++) {
+    emptyString += (deliveryOrderItemsArray[i].item)
+    if (deliveryOrderItemsArray[i] != deliveryOrderItemsArray[deliveryOrderItemsArray.length-1]) {
+      (emptyString += ", ");
+    }
+  }
+  return emptyString
+  //return emptyString //output: burgerblt sandwichrueben
+                      //if use emptyString again, make sure to change var in loop
+}
 
 
 module.exports = {
   takeOrder,
-  // refundOrder,
-  // listItems,
+  refundOrder,
+  listItems,
   // searchOrder
 }
